d64ead8aacdec259fd0e24990ad33f6737d73a76
Madhan T <Madhan.T@EI-LAP-9297.local>
2026-03-02 12:31:54 +0530

    serviceaccount key changes

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./rest-hosting-firebase-adminsdk-fbsvc-2a9948f401.json');
-
+const serviceAccount = require('./etc/secrets/rest-hosting-firebase-adminsdk-fbsvc-2a9948f401.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
